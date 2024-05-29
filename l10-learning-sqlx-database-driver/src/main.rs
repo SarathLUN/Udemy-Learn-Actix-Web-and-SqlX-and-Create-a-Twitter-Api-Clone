361a7caca542ba145b5840427b26d6808a48137a
SarathLUN <lun.sarath@gmail.com>
2024-05-29 13:35:12 +0700

    finish Lesson 10, pending handle sql transaction.

--- l10-learning-sqlx-database-driver/src/main.rs
+++ l10-learning-sqlx-database-driver/src/main.rs
@@ -5,35 +5,50 @@ use sqlx::{self, FromRow};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let conn = sqlx::postgres::PgPool::connect("postgres://tester:tester@localhost:5432/tester").await.unwrap();
-    // select query
-    let result = sqlx::query_as::<_, Profile>("select * from profile where id = $1")
-        .bind(2)
-        .fetch_one(&conn)
+    let conn = sqlx::postgres::PgPool::connect("postgres://tester:tester@localhost:5432/tester")
+        .await
+        .unwrap();
+
+    let mut tx = conn.begin().await.unwrap();
+
+    let insert_result = sqlx::query_as::<_, EntityId>("insert into profile (user_name, full_name) values ($1, $2) returning id")
+        .bind(Username().fake::<String>())
+        .bind(format!("{} {}", FirstName().fake::<String>(), LastName().fake::<String>()))
+        .fetch_one(&mut tx)
         .await;
-    println!("{:?}", result.unwrap());
-    // insert query
-    let result2 = sqlx::query_as::<_, EntityId>("insert into message (user_id, body, likes) values ($1, $2, $3) returning id")
-        .bind(2)
-        .bind("hello world")
-        .bind(10)
-        .fetch_one(&conn)
+
+    let query_result = sqlx::query_as::<_, Profile>("select * from profile where id = $1")
+        //.bind(243245645)
+        .bind(insert_result.unwrap().id)
+        .fetch_one(&mut tx)
         .await;
-    println!("{:?}", result2.unwrap());
-    // query with transaction
+
+    match query_result {
+        Ok(profile) => {
+            println!("Profile: {:?}", profile);
+            _ = tx.commit().await;
+        },
+        Err(_) => {
+            println!("Error: profile {} not found", 24324);
+            let rollback_result = tx.rollback().await;
+            println!("Rollback result: {:?}", rollback_result);
+        }
+    };
+
     Ok(())
 }
 
 #[derive(FromRow, Deserialize, Serialize, Debug)]
 struct EntityId {
-    id: i64,
+    pub id: i64
 }
 
+#[allow(unused)]
 #[derive(FromRow, Deserialize, Serialize, Debug)]
 struct Profile {
     pub id: i64,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub user_name: String,
-    pub full_name: String,
+    pub full_name: String
 }
